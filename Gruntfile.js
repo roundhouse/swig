99fdd8ffe694177b95f5f2e27a1adf96567e08c8
james <zetlen@gmail.com>
2013-12-03 08:35:54 -0600

    gruntfile now builds, tests next

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,64 @@
-"use strict";
+(function() {
+    "use strict";
 
-module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-    });
+    module.exports = function(grunt) {
+        grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+            tmp: 'tmp',
+            filenames: {
+                dist: 'dist/swig.js',
+                min: 'dist/swig.min.js',
+                sourceMap: 'dist/swig.js.map'
+            },
+            clean: {
+                dist: {
+                    src: ['dist']
+                },
+                tmp: {
+                    src: ['<%= tmp %>']
+                }
+            },
+            browserify: {
+                dist: {
+                    files: {
+                        '<%= filenames.dist %>': ['browser/index.js']
+                    }
+                }
+            },
+            uglify: {
+                dist: {
+                    options: {
+                        comments: true,
+                        warnings: false,
+                        sourceMap: '<%= filenames.sourceMap %>'
+                    },
+                    src: '<%= filenames.dist %>',
+                    dest: '<%= filenames.min %>'
+                }
+            },
+            concat: {
+                options: {
+                    banner: grunt.file.read('browser/comments.js')
+                },
+                dist: {
+                    src: ['<%= filenames.dist %>'],
+                    dest: '<%= filenames.dist %>'
+                },
+                min: {
+                    src: ['<%= filenames.min %>'],
+                    dest: '<%= filenames.min %>'
+                }
+            }
+        });
 
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-mocha');
-};
+        grunt.loadNpmTasks('grunt-browserify');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-mocha');
+
+        grunt.registerTask('browser', ['browserify', 'uglify', 'concat']);
+        grunt.registerTask('build', ['clean','browser']);
+
+    };
+})();
