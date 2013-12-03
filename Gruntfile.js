aac43802c5dacd438dacf7a2640c17130df9ea76
james <zetlen@gmail.com>
2013-12-03 15:45:14 -0600

    adding tests to grunt, mmkay

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,26 @@
 (function () {
   "use strict";
 
+  var browserTests = [
+    "tests/comments.test.js",
+    "tests/filters.test.js",
+    "tests/tags.test.js",
+    "tests/variables.test.js",
+    "tests/tags/autoescape.test.js",
+    "tests/tags/else.test.js",
+    "tests/tags/filter.test.js",
+    "tests/tags/for.test.js",
+    "tests/tags/if.test.js",
+    "tests/tags/macro.test.js",
+    "tests/tags/raw.test.js",
+    "tests/tags/set.test.js",
+    "tests/tags/spaceless.test.js",
+    "tests/basic.test.js"
+  ];
+
   module.exports = function (grunt) {
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      tmp: 'tmp',
       filenames: {
         dist: 'dist/swig.js',
         min: 'dist/swig.min.js',
@@ -14,14 +30,19 @@
         dist: {
           src: ['dist']
         },
-        tmp: {
-          src: ['<%= tmp %>']
+        browsertests: {
+          src: ['<%= concat.browsertests.dest %>']
         }
       },
       browserify: {
         dist: {
           files: {
             '<%= filenames.dist %>': ['browser/index.js']
+          }
+        },
+        browsertests: {
+          files: {
+            'browser/test/tests.js': '<%= concat.browsertests.dest %>'
           }
         }
       },
@@ -47,6 +68,38 @@
         min: {
           src: ['<%= filenames.min %>'],
           dest: '<%= filenames.min %>'
+        },
+        browsertests: {
+          src: browserTests,
+          dest: 'tests/browser.js'
+        }
+      },
+      mochaTest: {
+        test: {
+          options: {
+            reporter: 'dot',
+          },
+          src: ['./tests/**/*.test.js']
+        }
+      },
+      'regex-replace': {
+        browsertests: {
+          src: ['<%= concat.browsertests.dest %>'],
+          actions: [{
+            name: 'bin',
+            search: /\.\.\/\.\.\/lib/g,
+            replace: '../lib',
+            flags: 'g'
+          }]
+        }
+      },
+      mocha: {
+        test: {
+          options: {
+            reporter: 'Dot',
+            run: true
+          },
+          src: ['browser/test/index.html']
         }
       }
     });
@@ -55,10 +108,31 @@
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-regex-replace');
 
-    grunt.registerTask('browser', ['browserify', 'uglify', 'concat']);
-    grunt.registerTask('build', ['clean', 'browser']);
-
+    grunt.registerTask('browser', [
+      'browserify',
+      'uglify',
+      'concat:dist',
+      'concat:min'
+    ]);
+    grunt.registerTask('build', [
+      'clean',
+      'browser'
+    ]);
+    grunt.registerTask('default', [
+      'build',
+      'mochaTest'
+    ]);
+    grunt.registerTask('test', 'mochaTest');
+    grunt.registerTask('test-browser', [
+      'concat:browsertests',
+      'regex-replace:browsertests',
+      'browserify:browsertests',
+      'clean:browsertests',
+      'mocha'
+    ]);
   };
 }());
