7b58ea646d48169752e42bd9ae6f59c7de4413df
james <zetlen@gmail.com>
2013-12-20 11:28:59 -0600

    UMD stops looking for window-define and accepts a local one

--- browser/index.js
+++ browser/index.js
@@ -1,7 +1,7 @@
 var swig = require('../lib/swig');
 
-if (typeof window.define === 'function' && typeof window.define.amd === 'object') {
-  window.define('swig', [], function () {
+if (typeof define === 'function' && typeof define.amd === 'object') {
+  define('swig', [], function () {
     return swig;
   });
 } else {
--- dist/swig.js
+++ dist/swig.js
@@ -1,10 +1,10 @@
-/*! Swig v<%= pkg.version %> | https://paularmstrong.github.com/swig | @license https://github.com/paularmstrong/swig/blob/master/LICENSE */
+/*! Swig v1.2.0 | https://paularmstrong.github.com/swig | @license https://github.com/paularmstrong/swig/blob/master/LICENSE */
 /*! DateZ (c) 2011 Tomo Universalis | @license https://github.com/TomoUniversalis/DateZ/blob/master/LISENCE */
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var swig = require('../lib/swig');
 
-if (typeof window.define === 'function' && typeof window.define.amd === 'object') {
-  window.define('swig', [], function () {
+if (typeof define === 'function' && typeof define.amd === 'object') {
+  define('swig', [], function () {
     return swig;
   });
 } else {
